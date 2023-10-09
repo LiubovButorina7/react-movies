5b82eb536e6f746353225f6188c4452aae547c8c
liubovbutorina <liubovbutorina7@gmail.com>
2023-10-09 17:46:22 +0300

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import { Header } from './layout/Header';
 import { Footer } from './layout/Footer';
 import { Main } from './layout/Main';
-import { Preloader } from './components/Preloader';
 
 function App() {
   return (
