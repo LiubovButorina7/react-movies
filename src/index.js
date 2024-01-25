6d9fa7a53cc9357c0d3648ed411c009de2e2c934
liubovbutorina <liubovbutorina7@gmail.com>
2024-01-25 19:22:11 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-movies'>
+    <HashRouter basename='/react-movies'>
       <div className="page">
         <App />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
