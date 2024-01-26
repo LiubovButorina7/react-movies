70559e73f0a09400e8f9284d5f7c047612d79999
liubovbutorina <liubovbutorina7@gmail.com>
2024-01-26 12:25:11 +0300

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { Header } from './layout/Header';
 import { Footer } from './layout/Footer';
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Homepage } from './pages/Homepage';
 import { AboutMovie } from './pages/AboutMovie';
 import { NotFound } from './pages/NotFound';
 
-
 function App() {
   return (
-    <>
+    <div className="page">
       <Header />
       <main className="container content">
-      <Routes>
-        <Route path="/" element={ <Homepage/> } />
-        <Route path="/about/:id" element={ <AboutMovie/> } />
-        <Route path="*" element={ <NotFound/> } />
-      </Routes>
+        <BrowserRouter basename='/react-movies'>
+          <Routes>
+            <Route path="/" element={ <Homepage/> } />
+            <Route path="/about/:id" element={ <AboutMovie/> } />
+            <Route path="*" element={ <NotFound/> } />
+          </Routes>
+        </BrowserRouter>
       </main>   
       <Footer />      
-    </>
+    </div>
   );
 }
 
